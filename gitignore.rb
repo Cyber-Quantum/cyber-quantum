01f02db06e0756515438da446e035276bf4918ba
Sirani McNeill <13967481+mcne65@users.noreply.github.com>
2019-12-10 15:05:13 +1100

    Update and rename gitignore to gitignore.rb

---
+++ gitignore.rb
@@ -0,0 +1,11 @@
+Dir.glob("lib/tasks/**/*.rake").each { |r| load r }
+
+desc "Build the static site"
+task :build => ["contributors:update", :repo_pages, :man] do
+  sh "middleman build --clean"
+end
+
+# Heroku runs assets:precompile during deploys
+namespace :assets do
+  task :precompile => :build
+end
